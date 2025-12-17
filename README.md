c7e74802cbee35d518fb732f8d154c135c83cbec
bgrey77 <bgreyesq@gmail.com>
2025-12-17 10:22:03 -0300

    Render Despliegue

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GestionAusencias
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mevnnzk2a*ia2s-d5x#@*@o$wkngkc3gs#v8acrweiai^os&yh
--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
+
 python Ausencias/manage.py collectstatic --no-input
 python Ausencias/manage.py migrate
-python manage.py init_users
+python Ausencias/manage.py init_users
